fb49883b50bef53f208c438f7834674119731d80
Gonçalo Pattenden <goncalopattenden@tecnico.ulisboa.pt>
2024-05-02 10:59:29 +0100

    feat: added basic 3DObject functions and keydown event for wire frame

--- js/main-script.js
+++ js/main-script.js
@@ -8,19 +8,42 @@ import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 /* GLOBAL VARIABLES */
 //////////////////////
 
+var camera, scene, renderer;
+
+var materials = [new THREE.MeshBasicMaterial({ color: 0xbbbbbb, wireframe: true }),
+                 new THREE.MeshBasicMaterial({ color: 0xffdd00, wireframe: true }),
+                 new THREE.MeshBasicMaterial({ color: 0xffaa00, wireframe: true})]
+
+var geometry, mesh;
 
 /////////////////////
 /* CREATE SCENE(S) */
 /////////////////////
-function createScene(){
+function createScene() {
     'use strict';
 
+    scene = new THREE.Scene();
+
+    scene.add(new THREE.AxesHelper(100));
+
+    createCrane(0, 0, 0);
 }
 
 //////////////////////
 /* CREATE CAMERA(S) */
 //////////////////////
 
+function createCamera() {
+    'use strict';
+    camera = new THREE.PerspectiveCamera(70,
+                                         window.innerWidth / window.innerHeight,
+                                         1,
+                                         1000);
+    camera.position.x = 250;
+    camera.position.y = 250;
+    camera.position.z = 250;
+    camera.lookAt(scene.position);
+}
 
 /////////////////////
 /* CREATE LIGHT(S) */
@@ -29,6 +52,48 @@ function createScene(){
 ////////////////////////
 /* CREATE OBJECT3D(S) */
 ////////////////////////
+
+function addCraneBase(obj, x, y, z) {
+    'use strict';
+
+    geometry = new THREE.BoxGeometry(20, 10, 20);
+    mesh = new THREE.Mesh(geometry, materials[0]);
+    mesh.position.set(x, y + 5, z);
+    obj.add(mesh);
+}
+
+function addCraneTower(obj, x, y, z) {
+    'use strict';
+
+    geometry = new THREE.BoxGeometry(10, 220, 10);
+    mesh = new THREE.Mesh(geometry, materials[1]);
+    mesh.position.set(x, y + 120, z);
+    obj.add(mesh);
+}
+
+function addCraneJib(obj, x, y, z) {
+    'use strict';
+
+    geometry = new THREE.BoxGeometry(10, 10, 260);
+    mesh = new THREE.Mesh(geometry, materials[1]);
+    mesh.position.set(x, y, z);
+    obj.add(mesh);
+}
+
+function createCrane(x, y, z) {
+    'use strict';
+
+    var crane = new THREE.Object3D();
+
+    addCraneBase(crane, 0, 0, 0);
+    addCraneTower(crane, 0, 0, 0);
+
+    scene.add(crane);
+
+    crane.position.x = x;
+    crane.position.y = y;
+    crane.position.z = z;
+}
 
 //////////////////////
 /* CHECK COLLISIONS */
@@ -59,7 +124,7 @@ function update(){
 /////////////
 function render() {
     'use strict';
-
+    renderer.render(scene, camera);
 }
 
 ////////////////////////////////
@@ -67,7 +132,19 @@ function render() {
 ////////////////////////////////
 function init() {
     'use strict';
+    renderer = new THREE.WebGLRenderer({
+        antialias: true
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor(0xFFFFFF);
+    document.body.appendChild(renderer.domElement);
 
+    createScene();
+    createCamera();
+
+    window.addEventListener("keydown", onKeyDown);
+
+    render();
 }
 
 /////////////////////
@@ -75,7 +152,9 @@ function init() {
 /////////////////////
 function animate() {
     'use strict';
+    render();
 
+    requestAnimationFrame(animate);
 }
 
 ////////////////////////////
@@ -92,6 +171,30 @@ function onResize() {
 function onKeyDown(e) {
     'use strict';
 
+    switch (e.keyCode) {
+    case 49: //1
+        //material.wireframe = !material.wireframe;
+        break;
+    case 50: //2
+        //material.wireframe = !material.wireframe;
+        break;
+    case 51: //3
+        //material.wireframe = !material.wireframe;
+        break;
+    case 52: //4
+        //material.wireframe = !material.wireframe;
+        break;
+    case 53: //5
+        //material.wireframe = !material.wireframe;
+        break;
+    case 54: //6
+        //material.wireframe = !material.wireframe;
+        break;
+    case 55: //7
+        for (let i = 0; i < materials.length; i++)
+            materials[i].wireframe = !materials[i].wireframe;   
+        break;
+    }
 }
 
 ///////////////////////
